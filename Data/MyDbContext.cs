ac3cbd9509a2a0b7ba647a73cfcf15177caf6815
nguyendung.do <nguyendung.do@anacle.com>
2023-03-28 17:42:27 +0800

    Added DB context and DB connection string.

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FlashcardLibrary.Data
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options): base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Flashcard> Flashcards { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 using FlashcardLibrary.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+string? dbConnectionString = builder.Configuration.GetConnectionString("MainDB");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(dbConnectionString));
 
 var app = builder.Build();
 
