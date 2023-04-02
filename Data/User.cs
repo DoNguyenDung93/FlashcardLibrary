3d28112b699c62e8c70828e035b8874eb85453f2
nguyendung.do <nguyendung.do@anacle.com>
2023-04-02 17:34:16 +0800

    Added User object

---
+++ Data/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FlashcardLibrary.Data
+{
+    public class User : Base
+    {
+        public User(string username, string password) 
+        {
+            this.ObjectName = username;
+            this.Password = password;
+        }
+        public string Password { get; set; }
+    }
+}
