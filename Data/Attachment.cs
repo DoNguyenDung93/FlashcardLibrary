9851ae16375d77b80823840ac8dfe72ed33362c9
nguyendung.do <nguyendung.do@anacle.com>
2023-04-02 17:34:03 +0800

    Added Attachment object, to contain meaning, pronunciation, synonym, antonym.

---
+++ Data/Attachment.cs
@@ -0,0 +1,21 @@
+﻿namespace FlashcardLibrary.Data
+{
+    public class Attachment : Base
+    {
+        public Attachment(int type) 
+        {
+            this.AttachmentType = type;
+        }
+
+        public int AttachmentType { get; set; }
+        public string? Example { get; set; }
+    }
+
+    public enum AttachmentTypeEnum
+    {
+        Meaning = 0,
+        Pronunciation = 1,
+        Synonym = 2,
+        Antonym = 3,
+    }
+}
--- Data/Flashcard.cs
+++ Data/Flashcard.cs
@@ -5,6 +5,15 @@
         public Flashcard(string ObjectName)
         {
             this.ObjectName = ObjectName;
+            this.Attachments = new();
         }
+
+        public List<Attachment> Attachments { get; set; }
+        public List<Attachment> Meanings { get => GetAttachments((int)AttachmentTypeEnum.Meaning); }
+        public List<Attachment> Pronunciations { get => GetAttachments((int)AttachmentTypeEnum.Pronunciation); }
+        public List<Attachment> Synonyms { get => GetAttachments((int)AttachmentTypeEnum.Synonym); }
+        public List<Attachment> Antonyms { get => GetAttachments((int)AttachmentTypeEnum.Antonym); }
+        
+        private List<Attachment> GetAttachments(int type) => Attachments.FindAll(x => x.AttachmentType == type).ToList();
     }
 }
